ed17ccee34fc6e240574a2b3774dc183c2c95f21
Leonardo <leonardo@example.com>
2026-03-20 20:58:29 -0500

    Class3: Arithmetic operators & buckles

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,16 @@
 void main() {
 
     // Java 10--
-    String greeting = "Hello World!";
-    int numberOfOptions = 3;
-    boolean mobilityIndicator = true;
-    float numeroPi = 3.14;
+    String greeting = "Hi Pola!";
+    float numberOne = 10;
+    float numberTwo = 3;
+    IO.println(numberOne/numberTwo);
+    int a = 10, b = 3;
+    System.out.println("a / b: "+ a / b);      // → ?
+    System.out.println("a % b: "+ a % b);      // → ?
+    System.out.println(a > b);      // → ?
+    System.out.println(b == 10);
+
 
     // Using vars Java 10+
     var greeting1 = "Hello World!";
@@ -19,20 +25,66 @@ void main() {
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format(greeting));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    //Declare a String
+    String leo = "leo";
+    String leo2 = "LEO";
+    if (leo2.equals(leo)) {
+        IO.println("Leo es igual");
     }
 
-    if (true) {
-        //TIP Press <shortcut actionId="Run"/> to run the code, and then press <shortcut actionId="Stop"/> to stop it.
+
+    int edad = 17;
+    IO.println("edad: "+ edad);
+
+
+    boolean tieneCC = false;
+    IO.println("La persona tiene CC? : " + tieneCC);
+
+    if (!tieneCC) {
         IO.println(greeting);
         throw new RuntimeException("This is an exception!");
     }
 
     callSomething();
 
+    String msg;
+    int num = 0;
+    if (num % 2 == 0) {
+        msg = "Par";
+    } else if (num % 3 == 0) {
+        msg = "Impar";
+    } else if (num % 4 == 0) {
+        msg = "Repetir";
+    } else if (num % 5 == 0) {
+        msg = "Repetir";
+    }
+
+    switch (num) {
+        case 0:
+            msg = "Par";
+            break;
+        case 1:
+            msg = "Impar";
+            break;
+        case 2:
+            msg = "Repetir";
+            break;
+        case 3:
+            msg = "Repetir";
+            break;
+        default:
+            msg = "No es par ni impar ni repetir";
+    }
+
+
+    String msg2 = (num % 2 == 0) ? "Par" : "Impar";
+    IO.println(msg);
+// mismo resultado ✅
+
+
+    while(true){
+
+    }
 
 }
 
