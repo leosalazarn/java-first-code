e0328027222df8df7f41de81aafa6a788367918b
Leonardo <leonardo@example.com>
2026-04-06 18:13:55 -0500

    Quite otra linea

--- src/lemaster/Main.java
+++ src/lemaster/Main.java
@@ -9,7 +9,6 @@ void main() {
     int a = 10, b = 3;
     System.out.println("a / b: "+ a / b);      // → ?
     System.out.println("a % b: "+ a % b);      // → ?
-    System.out.println(a > b);      // → ?
     System.out.println(b == 10);
 
 
