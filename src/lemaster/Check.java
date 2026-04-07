d9c58ad3b3e6d7896d2b95b280b96dbefc649bba
Leonardo <leonardo@example.com>
2026-04-07 10:44:33 -0500

    feat: Add Check class

---
+++ src/lemaster/Check.java
@@ -0,0 +1,18 @@
+package lemaster;
+
+public class Check {
+
+    int a = 90;
+    void main
+            () {
+        int b = 130;
+        IO.println("Local a: " + b);
+        IO.println("Field a: " + b);
+
+    }
+
+    void Main(){
+        IO.println("Field a: " + a);
+        IO.println("Field a: " + a);
+    }
+}
